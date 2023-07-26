fff8d33b4ac1a28066268d9cc40333a4363cf119
Quentin <rousseltin@gmail.com>
2023-07-26 10:51:36 +0200

    Fix: Navigation component: Place the ancre inside de list items instead of wrapping it

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -10,15 +10,18 @@ function Navigation() {
       {token && <Logout />}
 
       <ul className="navigation__list">
-        <a href="#about">
-          <li className="navigation__list__item">A propos</li>
-        </a>
-        <a href="#projects">
-          <li className="navigation__list__item"> Projets</li>
-        </a>
-        <a href="#contact">
-          <li className="navigation__list__item">Contact</li>
-        </a>
+        <li className="navigation__list__item">
+          <a href="#about">A propos </a>
+        </li>
+
+        <li className="navigation__list__item">
+          {" "}
+          <a href="#projects">Projets</a>
+        </li>
+
+        <li className="navigation__list__item">
+          <a href="#contact">Contact</a>
+        </li>
       </ul>
     </nav>
   );
